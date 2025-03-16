25101696900b4b7cf63205fedc4222a580f1aa08
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2025-03-16 14:48:23 +0530

    refactor

---
+++ samples/ConsoleApp/NativeArray.cs
@@ -0,0 +1,53 @@
+﻿using Cosmic.Allocator;
+
+unsafe ref struct NativeArray<T> : IDisposable where T : unmanaged
+{
+    Arena arena;
+    public int Length { get; private set; }
+    private int ItemSize { get; }
+
+    public NativeArray()
+    {
+        ItemSize = sizeof(T);
+        arena = ArenaManager.Create((nuint)(ItemSize)*4);
+    }
+
+    public void Add(T data)
+    {
+        Span<T> t = arena.Alloc<T>(1);
+        t[0] = data;
+        Length++;
+    }
+
+    T GetItem(int index)
+    {
+        var nestedArena = arena.AsPointer()->GetArenaByItemIndex(index, ItemSize, out int byteOffset);
+
+        if (nestedArena == SafeHandle<Arena>.Zero)
+            throw new Exception("Invalid Index");
+
+        return nestedArena.AsPointer()->DataRegion.GetItem<T>(byteOffset / ItemSize);
+
+    }
+
+    public void Set(int index,T item)
+    {
+        var nestedArena = arena.AsPointer()->GetArenaByItemIndex(index, ItemSize, out int byteOffset);
+
+        if (nestedArena == SafeHandle<Arena>.Zero)
+            throw new Exception("Invalid Index");
+
+        nestedArena.AsPointer()->DataRegion.SetItem<T>(byteOffset / ItemSize,item);
+    }
+
+    public void Dispose()
+    {
+        arena.Dispose();
+    }
+
+    public T this[Index index]
+    {
+        get => GetItem(index.GetOffset(Length));
+    }
+}
+
--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
-﻿using Cosmic.Allocator;
-using System.Collections;
+﻿using System.Collections;
 
 {
 
@@ -18,57 +17,6 @@ using System.Collections;
     {
         var pt = pointArray[i];
         Console.WriteLine(pt.ToString());
-    }
-}
-
-unsafe ref struct NativeArray<T> : IDisposable where T : unmanaged
-{
-    Arena arena;
-    public int Length { get; private set; }
-    private int ItemSize { get; }
-
-    public NativeArray()
-    {
-        ItemSize = sizeof(T);
-        arena = ArenaManager.Create((nuint)(ItemSize)*4);
-    }
-
-    public void Add(T data)
-    {
-        Span<T> t = arena.Alloc<T>(1);
-        t[0] = data;
-        Length++;
-    }
-
-    T GetItem(int index)
-    {
-        var nestedArena = arena.AsPointer()->GetArenaByItemIndex(index, ItemSize, out int byteOffset);
-
-        if (nestedArena == SafeHandle<Arena>.Zero)
-            throw new Exception("Invalid Index");
-
-        return nestedArena.AsPointer()->DataRegion.GetItem<T>(byteOffset / ItemSize);
-
-    }
-
-    public void Set(int index,T item)
-    {
-        var nestedArena = arena.AsPointer()->GetArenaByItemIndex(index, ItemSize, out int byteOffset);
-
-        if (nestedArena == SafeHandle<Arena>.Zero)
-            throw new Exception("Invalid Index");
-
-        nestedArena.AsPointer()->DataRegion.SetItem<T>(byteOffset / ItemSize,item);
-    }
-
-    public void Dispose()
-    {
-        arena.Dispose();
-    }
-
-    public T this[Index index]
-    {
-        get => GetItem(index.GetOffset(Length));
     }
 }
 
